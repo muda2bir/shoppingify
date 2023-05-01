47ebbd81cf0942cb64b9c8d1832e3680572ed253
Mudabbir Hussain Momin <mudabbir.hm@gmail.com>
2023-05-01 16:21:06 +0530

    fixed database connection issue

--- src/sequelize/index.ts
+++ src/sequelize/index.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
-// const DATABASE_NAME = process.env.DATABASE_NAME as string;
-// const DATABASE_USERNAME = process.env.DATABASE_USERNAME as string;
-// const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD as string;
-// const DATABASE_HOSTNAME = process.env.DATABASE_HOSTNAME as string;
+const DATABASE_HOSTNAME = process.env.DATABASE_HOSTNAME as string;
 const DATABASE_URL = process.env.DATABASE_URL as string;
 
-export const sequelize = new Sequelize(DATABASE_URL);
+export const sequelize = new Sequelize(DATABASE_URL, {
+  host: DATABASE_HOSTNAME,
+  dialect: "postgres",
+});
